81fc413084813f033357d7aa13e4a8c3739e0202
rogeriokoski <rogeriokoski@hotmail.com>
2023-04-01 10:43:31 -0300

    Using env variables

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -1,7 +1,9 @@
 import fetch from "node-fetch";
 
 function forecast(lat, long, callback) {
-  const url = `http://api.weatherstack.com/current?access_key=c279f533edd6461e678fe1894611f921&query=${lat},${long}`;
+  // const url = `http://api.weatherstack.com/current?access_key=c279f533edd6461e678fe1894611f921&query=${lat},${long}`;
+  const url = `http://api.weatherstack.com/current?access_key=${process.env.WS_API_KEY}&query=${lat},${long}`;
+  console.log("URL for weatherstack: ", url);
   fetch(url)
     .then((response) => {
       return response.json();
@@ -12,7 +14,8 @@ function forecast(lat, long, callback) {
         // throw new Error("Location not found. Try a different search");
       } else {
         const { name, country } = data.location;
-        const { weather_descriptions, temperature, precip, humidity } = data.current;
+        const { weather_descriptions, temperature, precip, humidity } =
+          data.current;
         console.log(data);
         callback(
           undefined,
@@ -22,7 +25,7 @@ function forecast(lat, long, callback) {
             description: weather_descriptions[0],
             temperature: temperature,
             precipChance: precip,
-            humidity: humidity
+            humidity: humidity,
           }
           // `In ${name} the weather is ${weather_descriptions[0]}, with ${temperature} Celsius and a ${precip}% chance of rain`
         );
